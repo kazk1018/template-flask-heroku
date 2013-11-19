0bcba07090c2b71195e417ac9bd8a482aaf6000c
Kazuki <kazuki.t.1018@gmail.com>
2013-11-19 23:40:52 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# configuration
+DEBUG = True
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# run app
+if __name__ == '__main__':
+    app.run()
